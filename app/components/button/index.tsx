aac19f3aa34be3235e254df6342fe783fd128b59
MAYKON MAYKON <gustavo.bordignon@outlook.com>
2024-03-14 16:41:06 -0300

    fix widthButton

--- app/components/button/index.tsx
+++ app/components/button/index.tsx
@@ -8,7 +8,7 @@ const Button = ({text,width}:ButtonProps) => {
     return(
 
         <a className=
-        {`w-full bg-[#4CAF50] text-center py-1 hover:opacity-50 w-[${width}px] rounded-md cursor-pointer uppercase`}>
+        {`bg-[#4CAF50] text-center py-1 hover:opacity-50 w-[${width}px] rounded-md cursor-pointer uppercase`}>
             {text}
         </a>
 
