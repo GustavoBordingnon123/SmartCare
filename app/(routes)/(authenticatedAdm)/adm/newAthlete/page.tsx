01cfa7cf211d4cc19a9d2f6090835bf069d0079a
MAYKON MAYKON <gustavo.bordignon@outlook.com>
2024-04-25 16:57:14 -0300

    start the new input validators

--- app/(routes)/(authenticatedAdm)/adm/newAthlete/page.tsx
+++ app/(routes)/(authenticatedAdm)/adm/newAthlete/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react'
 
 import Button from '@/app/components/ui/separator/button';
@@ -5,25 +7,24 @@ import Input from '@/app/components/ui/separator/input';
 
 const Profile = () => {
 
-  const inputCampos = [
-    {
-      text: 'Nome',
-      regex: undefined
-    },
-    {
-      text: 'Email',
-      regex: undefined
-    },
-    {
-      text: 'Cpf',
-      regex: undefined
-    },
-    {
-      text: 'Primeira senha',
-      regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
-    },
-  ]
-  
+    const inputCampos = [
+      {
+        text: 'Nome',
+        regex: undefined
+      },
+      {
+        text: 'Email',
+        regex:undefined
+      },
+      {
+        text: 'Cpf',
+        regex:undefined
+      },
+      {
+        text: 'Primeira senha',
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+      },
+    ]
 
     return (
       <div className="flex flex-col gap-5 mx-10 py-8">
@@ -34,7 +35,12 @@ const Profile = () => {
 
             <div className='flex gap-5 flex-col justify-center items-start ml-5 mt-5 w-full'>
                 {inputCampos.map((opcao, index) => (
-                  <Input label={`${opcao.text}`} placeholder={`${opcao.text}`} regex={opcao.regex} labelColor='white' />
+                  <Input 
+                    label={`${opcao.text}`} 
+                    placeholder={`${opcao.text}`} 
+                    regex={opcao.regex} 
+                    labelColor='white' 
+                  />
                 ))}
 
                 <Button text='Criar' href='/adm/athletes'/>
--- app/components/ui/separator/input/index.tsx
+++ app/components/ui/separator/input/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { Check, X } from "lucide-react";
 import React, { useState } from "react";
 
 interface InputProps {
     label: string;
     placeholder: string;
     regex?: RegExp;
+    icon?:any;
     labelColor?: string;
 }
 
@@ -19,9 +21,8 @@ const Input = ({ label, placeholder, regex, labelColor = "black" }: InputProps) 
 
         if (regex) {
             setIsValid(regex.test(value));
-        } else {
-            setIsValid(true); 
         }
+        
     };
 
     return (
@@ -29,15 +30,24 @@ const Input = ({ label, placeholder, regex, labelColor = "black" }: InputProps) 
             <label htmlFor={label} className={`text-${labelColor}`}>
                 {label}
             </label>
-            <input
-                id={label}
-                placeholder={placeholder}
-                value={inputValue}
-                onChange={handleChange}
-                className={`bg-[#D9D9D9] w-[80%] rounded-md py-1 px-2 text-black border-2 ${
-                    !isValid ? "border-red-500" : "border-none"
-                }`}
-            />
+            <div className="flex w-full gap-2">
+                <input
+                    id={label}
+                    placeholder={placeholder}
+                    value={inputValue}
+                    onChange={handleChange}
+                    className={`bg-[#D9D9D9] w-[80%] rounded-md py-1 px-2 text-black border-2 ${
+                        !isValid ? "border-red-500" : "border-none"
+                    }`}
+                />
+                {!isValid ? (
+                    <X  size={32} className="relative right-12 top-[2px] text-red-500"/>
+                ): (
+                    <Check  size={32} className="relative right-12 top-[2px] text-emerald-500"/>
+                )
+                }   
+            </div>
+        
         </div>
     );
 };
